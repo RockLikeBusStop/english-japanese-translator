3ec0d163d47d944a974b70e39d09a0ffe0dada14
RockLikeBusStop <arasam1067@gmail.com>
2023-10-09 14:57:53 -0400

    updated data processing

--- process_data.py
+++ process_data.py
@@ -1,4 +1,5 @@
 import constants as c
+import multiprocessing as mp
 import pickle
 import re
 from JapaneseTokenizer import JumanppWrapper
@@ -6,33 +7,47 @@ from Lang import Lang
 
 
 def normalize_en(s):
-    """Processes an English string by removing non-alphabetical characters (besides .!?)."""
+    """ Processes an English string by removing non-alphabetical characters (besides .!?).
+    """
     s = s.lower().strip()
+    s = re.sub(r"([.!?])", r" \1", s)
     s = re.sub(r"[^\w.!?]+", r" ", s, flags=re.UNICODE)
     return s
 
 
 def normalize_ja(s, segmenter):
-    """Processes a Japanese character by removing non-word characters and separating tokens with spaces."""
+    """ Processes a Japanese string by removing non-word characters and separating tokens with spaces.
+    """
+    s = s.strip()
     s = re.sub(r"[^\w.!?。]+", r" ", s, flags=re.UNICODE)
-    s = " ".join(
-        segmenter.tokenize(
-            sentence=s, is_feature=False, is_surface=True
-        ).convert_list_object()
-    )
+    s = " ".join(segmenter.tokenize(sentence=s, is_feature=False, is_surface=True).convert_list_object())
     s = re.sub("\s+", " ", s).strip()
     return s
 
 
-def read_langs(en_file, ja_file):
-    """Reads corpuses and returns a Lang object for each language and all normalized sentence pairs."""
+def normalize(en_lines, ja_lines):
+    """ Process lists of both English and Japanese strings.
+    """
     segmenter = JumanppWrapper()
-    en_lines = open(en_file).read().strip().split("\n")
-    ja_lines = open(ja_file).read().strip().split("\n")
-    pairs = [
-        [normalize_en(l1), normalize_ja(l2, segmenter)]
-        for l1, l2 in zip(en_lines, ja_lines)
+    return [[normalize_en(l1), normalize_ja(l2, segmenter)] for l1, l2 in zip(en_lines, ja_lines)]
+
+
+def read_langs(en_file, ja_file, n_processes=4):
+    """ Reads corpuses and returns a Lang object for each language and all normalized sentence pairs.
+    """
+    en_lines = open(en_file).read().split("\n")
+    ja_lines = open(ja_file).read().split("\n")
+
+    pool = mp.Pool(processes=n_processes)
+    interval = len(en_lines) // n_processes
+    results = [
+        pool.apply_async(
+            normalize, args=(en_lines[i * interval:(i + 1) * interval], ja_lines[i * interval:(i + 1) * interval]))
+        for i in range(n_processes)
     ]
+    pairs = []
+    for p in results:
+        pairs += p.get()
 
     en = Lang("en")
     ja = Lang("ja")
@@ -40,23 +55,45 @@ def read_langs(en_file, ja_file):
     return en, ja, pairs
 
 
-def filter_pair(p):
-    """Filter out sentences that exceed maximum length."""
-    return len(p[0].split(" ")) < c.MAX_LENGTH and len(p[1].split(" ")) < c.MAX_LENGTH
+def filter_pair_by_vocab(p, lang1, lang2):
+    """ Filter out sentences if they do not contain words in vocab.
+    """
+    s1 = p[0].split(" ")
+    s2 = p[1].split(" ")
+    for word in s1:
+        if word not in lang1.word2index:
+            return False
+    for word in s2:
+        if word not in lang2.word2index:
+            return False
+    return True
 
 
-def filter_pairs(pairs):
-    return [pair for pair in pairs if filter_pair(pair)]
+def filter_pair_by_len(p, maxlen=c.MAX_LENGTH):
+    """ Filter out sentences if they are greater than maximum length.
+    """
+    return len(p[0].split(" ")) < maxlen and len(p[1].split(" ")) < maxlen
+
+
+def filter_vocab(lang, min_words=2):
+    """ Filters out words from Lang with counts less than min_words in place.
+    """
+    remove_words = [key for key in lang.word2count.keys() if lang.word2count[key] < min_words]
+    for word in remove_words:
+        lang.remove_word(word)
 
 
 if __name__ == "__main__":
     en, ja, pairs = read_langs(c.EN_PATH, c.JA_PATH)
     print("Number of sentences:", len(pairs))
-    pairs = filter_pairs(pairs)
-    print("Number of trimmed sentences:", len(pairs))
+    pairs = [pair for pair in pairs if filter_pair_by_len(pair, c.MAX_LENGTH)]
     for pair in pairs:
         en.add_sentence(pair[0])
         ja.add_sentence(pair[1])
+    filter_vocab(en, c.MIN_VOCAB_WORDS)
+    filter_vocab(ja, c.MIN_VOCAB_WORDS)
+    pairs = [pair for pair in pairs if filter_pair_by_vocab(pair, en, ja)]
+    print("Number of trimmed sentences:", len(pairs))
     print("Number of {} words: {}".format(en.name, en.n_words))
     print("Number of {} words: {}".format(ja.name, ja.n_words))
 
